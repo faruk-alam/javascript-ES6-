c1583187be98d88530c85713b311fb14c67ccc20
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-12-13 21:37:07 +0100

    Add files via upload

---
+++ ES6 Intro and variable declaration/app.js
@@ -0,0 +1,19 @@
+// using var keyword
+var x = 6;
+if(true){
+  var  x = 5;
+}
+console.log(x);
+
+// similar code using let key wordBreak: 
+let y = 6;
+if(true){
+  let  y = 5;
+}
+console.log(y);
+
+// using const keyword
+const PI = 3.14;
+//const PI = 10;-show error
+console.log(PI); 
+
