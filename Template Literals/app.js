ee3d24b6fa9ba6d9a3d1c1da6ed90411af2e528b
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-12-15 11:06:06 +0100

    Add files via upload

---
+++ Template Literals/app.js
@@ -0,0 +1,9 @@
+let str = `The quick brown fox 
+jumps over the lazy dog.`;
+console.log(str);
+
+
+let a = 10;
+let b = 20;
+let result = (`the sum of ${a} and the sum of ${b} is ${a + b}`);
+console.log(result);
