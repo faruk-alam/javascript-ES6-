47dee14e8958cc496445e245d9a10e9ff384ef8b
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-12-19 19:16:49 +0100

    Add files via upload

---
+++ for--of loop/main.js
@@ -0,0 +1,30 @@
+// Iterating over array
+let letters = ["a","b","c","d","e"];
+for(let letter of letters){
+    console.log(letter);
+}
+
+// Iterating over another type of array
+const students = ['John', 'Sara', 'Jack'];
+
+// using for...of
+for ( let element of students ) {
+
+    // display the values
+    console.log(element);
+}
+
+// Iterating over string
+let greet = "Hello World!";
+    
+for(let character of greet){
+    console.log(character);
+}
+
+// Iterating over string(i)
+const string = 'code';
+
+// using for...of loop
+for (let i of string) {
+    console.log(i);
+}
